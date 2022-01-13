294bef05d18c526ded181eec096259babf71445f
Keshvir <katugh@shoprite.co.za>
2022-01-13 16:22:48 +0200

    update
    Updated script to include function to get user name and write this to the archive in the Reporting Documents folder in Onedrive

--- Scripts.R
+++ Scripts.R
@@ -1,18 +1,32 @@
+library(stringr)
 library(tidyverse)
 
-SAPAnalyticsReport <- read_csv("C:/Users/katugh/Documents/GIT Repo/Daily Reports/Input/SAPAnalyticsReport(TicketStatusReport).csv") %>%
-                      select(-'Service Level') %>%
-                      rename('Service Level' = ...4)
+#function to get username
+user <- function(wd){
+  user_name <- wd %>%
+    str_remove("C:/Users/") 
+  
+  substring(user_name,1, regexpr("/", user_name)-1)
+}
+
+dt <- Sys.Date()
+
+SAPAnalyticsReport <- read_csv("Input/SAPAnalyticsReport(TicketStatusReport).csv") %>%
+  select(-'Service Level') %>%
+  rename('Service Level' = ...4)
+
+username <- user(getwd())
+write_csv(SAPAnalyticsReport, paste0("C:/Users/",username,"/OneDrive - Shoprite Checkers (Pty) Limited/Reporting Files and Documents/Ticket Status Report/Archive/SAPAnalyticsReport(TicketStatusReport ",dt,").csv"))
 
 TicketStatus <- SAPAnalyticsReport %>%
-      filter(`Ticket Type` == "Careline") %>%
-      select(Agent, `Ticket ID`, `Service Level`, `Changed On`, `Service Category`, `Incident Category`, Object, Origin, `Case Title`,
-           Status, Priority, `Created On`, `Completion Date`)
+  filter(`Ticket Type` == "Careline") %>%
+  select(Agent, `Ticket ID`, `Service Level`, `Changed On`, `Service Category`, `Incident Category`, Object, Origin, `Case Title`,
+         Status, Priority, `Created On`, `Completion Date`)
 
 UnassignedTickets <- SAPAnalyticsReport %>%
-      select(Agent, `Ticket ID`, `Ticket Type`, `Changed On`, `Service Category`, `Incident Category`, Object, Origin, `Case Title`,
-          Status, Priority, `Created On`, `Completion Date`)
+  select(Agent, `Ticket ID`, `Ticket Type`, `Changed On`, `Service Category`, `Incident Category`, Object, Origin, `Case Title`,
+         Status, Priority, `Created On`, `Completion Date`)
 
-write_csv(TicketStatus, "C:/Users/katugh/Documents/GIT Repo/Daily Reports/Output/TicketStatus.csv")
+write_csv(TicketStatus, "Output/TicketStatus.csv")
 
-write_csv(UnassignedTickets, "C:/Users/katugh/Documents/GIT Repo/Daily Reports/Output/UnassignedTickets.csv")
+write_csv(UnassignedTickets, "Output/UnassignedTickets.csv")
